866d9f1586642fe199bdf57213d76a9defc48beb
Egor4ik325 <zorinegor352@gmail.com>
2021-08-04 15:25:06 +0300

    Add parcels taken

--- check.py
+++ check.py
@@ -18,15 +18,24 @@ def main():
     ready_payload = {
         'func': 'loyalty.dbo.Parcels_GetParcelsShop_new',
         'id_courier': id_courier,
-        'date_from': '20210730',
-        'date_to': '20210805'
+        'date_from': '20210801',
+        'date_to': '20210807'
+    }
+
+    taken_payload = {
+        'func': 'loyalty.dbo.Parcels_GetParcelsShop',
+        'id_courier': id_courier,
+        'date_from': '20210801',
+        'date_to': '20210807'
     }
 
     r1 = get(api_url, prepare_payload, headers=headers)
     r2 = get(api_url, ready_payload, headers=headers)
+    r3 = get(api_url, taken_payload, headers=headers)
 
     prepare = r1.json()[0]
     ready = r2.json()[0]
+    taken = r3.json()[0]
 
     def print_parcel(p):
         distance = int(float(p['distance']))
@@ -44,6 +53,13 @@ def main():
     print("Parcels ready: ")
     if len(ready) > 0:
         for p in ready:
+            print_parcel(p)
+    else:
+        print("- No")
+
+    print("Parcels taken: ")
+    if len(ready) > 0:
+        for p in taken:
             print_parcel(p)
     else:
         print("- No")
